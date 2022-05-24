57b0929d023fa7eaebc6e082046a38ee39866868
Fabio Teixeira <fabio.teixeira@ifsp.edu.br>
2022-05-24 18:36:30 -0300

    Enviar para o GITHUB

--- config/passport.js
+++ config/passport.js
@@ -8,8 +8,8 @@ module.exports = function() {
     var Usuario = mongoose.model('Usuario');
 
     passport.use(new GitHubStrategy({
-        clientID: 'de4c2e6477ed3b643981',
-        clientSecret: '91a0a6bb119aa2f7c31dab3622b984ff687e88b4',
+        clientID: 'client ID',
+        clientSecret: 'client Secret',
         callbackURL: 'https://mean-stack-14-aula.herokuapp.com/auth/github/callback'
     }, function(accessToken, refreshToken, profile, done) {
 
